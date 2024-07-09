76e29423d40fc1ae4e514e6a8c0bd8c7e5642953
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-09 11:45:52 +0300

    Update baqir.py

--- baqir.py
+++ baqir.py
@@ -1,13 +1,10 @@
-import os
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return "Repthon working 😁"
+@app.route('/')
+def hello_world():
+    return 'Welcome To Source Repthon'
 
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+
+if __name__ == "__main__":
+    app.run()
